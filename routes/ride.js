14ac83c80ed9086380b303fdfaf695927b00d183
Isaac Donkor <isaac.kwakye.donkor@outlook.com>
2025-06-19 06:13:07 +0000

    Updated to a role based acceptance

--- routes/ride.js
+++ routes/ride.js
@@ -4,7 +4,7 @@ const Ride = require('../models/Ride');
 const { protect } = require('../middleware/authMiddleware');
 const getRouteInfo = require('../utils/maps');
 
-
+//Riders only: request a ride
 router.post('/request', async (req, res) => {
   const { riderId, pickup, dropoff } = req.body;
 
@@ -19,9 +19,22 @@ router.post('/request', async (req, res) => {
   res.json({ride,routeInfo});
 });
 
-router.post('/accept', async (req, res) => {
-  const { rideId, driverId } = req.body;
-  const ride = await Ride.findByIdAndUpdate(rideId, { driver: driverId, status: 'accepted' }, { new: true });
+//Drivers only: accept a ride
+router.post('/accept',protect,restriction('driver'), async (req, res) => {
+    const { rideId } = req.body;
+
+  if (!ride) {
+    return res.status(404).json({ message: 'Ride not found' });
+  }
+
+  if (ride.status !== 'requested') {
+    return res.status(400).json({ message: 'Ride already taken or completed' });
+  }
+
+  ride.driver = req.user._id;
+  ride.status = 'accepted';
+
+  await ride.save();
   res.json(ride);
 });
 

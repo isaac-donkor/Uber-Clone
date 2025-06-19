3cdfc855cc1036602eb65fb91e9a2cdc33541724
Isaac Donkor <isaac.kwakye.donkor@outlook.com>
2025-06-19 05:53:40 +0000

    Added route information

--- routes/ride.js
+++ routes/ride.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const Ride = require('../models/Ride');
+const { protect } = require('../middleware/authMiddleware');
+const getRouteInfo = require('../utils/maps');
+
 
 router.post('/request', async (req, res) => {
   const { riderId, pickup, dropoff } = req.body;
-  const ride = await Ride.create({ rider: riderId, pickup, dropoff });
-  res.json(ride);
+
+  const routeInfo = await getRouteInfo(pickup, dropoff);
+  
+  const ride = await Ride.create({ 
+     rider: riderId, 
+     pickup, 
+     dropoff,
+    ...routeInfo });
+
+  res.json({ride,routeInfo});
 });
 
 router.post('/accept', async (req, res) => {

06e9051484620ae7210a700dbe0402101ed4bb12
Isaac Donkor <isaac.kwakye.donkor@outlook.com>
2025-06-19 05:35:28 +0000

    Hashed Passwords

--- models/User.js
+++ models/User.js
@@ -1,12 +1,28 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: String,
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
   role: { type: String, enum: ['rider', 'driver'], required: true },
   location: {
     lat: Number,
     lng: Number
   }
 });
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Compare entered password with hashed
+userSchema.methods.matchPassword = function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
